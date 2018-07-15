d8400b36708d31100b128474759c6f157cd584ad
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2018-07-14 21:18:57 -0500

    Make profile accesible

--- src/views/Bio.js
+++ src/views/Bio.js
@@ -8,18 +8,18 @@ class Bio extends Component {
   render() {
     return (
       <div>
-        <img className="pull-left" width="25%" height="70%" src={images.profile}></img>
+        <img tabIndex="1" alt="Ana Maria profile image" className="pull-left" width="25%" height="70%" src={images.profile}></img>
         <div className="content">
             <h1 className="bio_title">Ana María Cárdenas</h1>
             <div className="bio_text">
-              <p>
+              <p tabIndex="2">
                 I finished a BS in Computer Science at <b>Universidad de Los Andes</b> I'm on my 4rth (and last) year of Design. 
               </p>
-              <p>
+              <p tabIndex="3">
                 I'm interested in visualization, game design, UX, Interfaces design and Education. 
               </p>
             </div>
-            <p>
+            <p tabIndex="4">
                 You can have a link of my CV here <a href={cv}>Ana Maria Cardenas CV</a>
             </p>
         </div>		
